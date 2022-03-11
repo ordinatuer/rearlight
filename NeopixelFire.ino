8e82d1e4f1c3be9e78917a9ed14e2b2a6f20441f
ordinatuer <ordnatuer@yandex.ru>
2022-03-11 22:52:16 +0300

    Слишком эпилептично получается

--- NeopixelFire.ino
+++ NeopixelFire.ino
@@ -2,17 +2,24 @@
 #include "Adafruit_NeoPixel.h"
  
 // Указываем, какое количество пикселей у нашей ленты.
-#define LED_COUNT 20
+#define LED_COUNT 10
  
 // Указываем, к какому порту подключен вход ленты DIN.
-#define LED_PIN 14 // 14 = A0
+// 21 = A7 для Nano
+#define LED_PIN 19 // 19 = A5
  
 // Создаем переменную strip для управления нашей лентой.
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+// начало пробега по диодам
 int iStart = 0;
+// направление пробега
 int iDirect = 1;
-int delayMs = 100;
+// задержка главного цикла (loop())
+int delayMs = 20;
+// момент последнего срабатывания программы
+unsigned long previosMs = 0;
+
  
 void setup()
 {
@@ -20,14 +27,26 @@ void setup()
   strip.begin();
 
   // устанавливаем яркость (до 255)
-  strip.setBrightness(150);
+  strip.setBrightness(200);
 
   randomSeed(analogRead(0));
 }
  
 void loop()
 {
-  if (LED_COUNT == iStart) {
+  // время исполнения текущей итерации loop()
+  unsigned long currentMs = millis();
+
+  // вместо delay() 
+  if (delayMs < (currentMs - previosMs)) {
+    previosMs = currentMs;
+
+    rgbSwitch();
+  }
+}
+
+void rgbSwitch() {
+    if (LED_COUNT == iStart) {
     iDirect = 0;
   }
   if (0 == iStart) {
@@ -40,15 +59,16 @@ void loop()
     iStart--;  
   }
   
-  // Включаем все светодиоды.
+  // Перебираем все светодиоды.
   for (int i = 0; i < LED_COUNT; i++)
-  {
-    if (i != iStart) {
-      // все огни
-      strip.setPixelColor(i, strip.Color(255, 0, 0));
+  { 
+    //if (i == iStart || i == (LED_COUNT - iStart)) {
+    if (i == iStart) {
+      // бегущий диод
+      strip.setPixelColor(i, strip.Color(250, 0, 100));
     } else {
-      // бегущий огонь
-      strip.setPixelColor(i, strip.Color(0, rand255(), rand255()));
+      // остальные, фоновые диоды
+      strip.setPixelColor(i, strip.Color(250, 0, 0));
     }
   }
   
@@ -56,9 +76,5 @@ void loop()
   strip.show();
   
   // Ждем (мс).
-  delay(delayMs);
-}
-
-int rand255() {
-  return random(0, 255);
+  //delay(delayMs);
 }
